4f5de12879e5721c960285b2205d0edad5fcddcd
Shreshtthh <shreshth013@gmail.com>
2024-10-11 00:05:46 +0530

    Added faculty names

---
+++ Faculty names/facnames.js
@@ -0,0 +1,62 @@
+export const faculty =[
+  {
+    fname:"Prof H.M. Suryawanshi",
+    post:"Director",
+    img:"",
+
+  },
+  {
+    fname:"Dr. Ashok Kumar",
+    post:"Dean Student Welfare",
+    img:""
+    
+  },
+  {
+    fname:"Dr. Pardeep Singh",
+    post:"Associate Dean (SA&S)",
+    img:""
+    
+  },
+  {
+    fname:"Dr Rajeev Kumar",
+    post:"FI",
+    img:"",
+    
+  },
+  {
+    fname:"Dr. Zareena J.M.",
+    post:"Female FI",
+    img:"",
+    
+  },
+  {
+    fname:"Dr. Niharika Gupta",
+    post:"AFI-Nritya",
+    img:"",
+    
+  },
+  {
+    fname:"Dr. Ajay Kumar Mallick",
+    post:"AFI Sangeet",
+    img:"",
+    
+  },
+  {
+    fname:"Dr. Swaraj Chowdhury",
+    post:"AFI-Abhinaya",
+    img:"",
+    
+  },
+  {
+    fname:"Dr. Meghna Sharma",
+    post:"AFI-Sahitya Kala",
+    img:"",
+    
+  },
+  {
+    fname:"Dr. Jeetandrasingh Maan",
+    post:"AFI-Nirvahana",
+    img:""
+    
+  }
+]
